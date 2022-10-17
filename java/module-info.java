5b18baedc8f68b401a181d046fbbafef3d5b36d2
Jakub Marcinkowski <jakub.marcinkowski.programista@gmail.com>
2022-10-17 17:56:44 +0200

    create project base

---
+++ java/module-info.java
@@ -0,0 +1,12 @@
+module Test1 {
+
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.web;
+
+    opens pl.jakubmarcinkowski;
+
+
+    opens pl.jakubmarcinkowski.view;
+}
---
+++ java/pl/jakubmarcinkowski/view/Base.java
@@ -0,0 +1,11 @@
+package pl.jakubmarcinkowski.view;
+
+import javafx.fxml.FXML;
+
+public class Base {
+
+    @FXML
+    public void buttonAction(){
+        System.out.println("clicked the button!!!!");
+    }
+}
